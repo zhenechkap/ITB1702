b881a6d5535277778b1fc781239bfcd2243ecbcf
Evgenia <zheneeeek@mail.ru>
2019-02-03 17:17:36 +0200

    upload
    fghj

---
+++ Week1/Week1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
