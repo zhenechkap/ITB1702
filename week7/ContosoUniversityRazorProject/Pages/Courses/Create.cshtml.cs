043a4a4217ef90d4f27f7026fb9a16bdd60ced6d
Evgenia <evgenia.pavlova1993@gmail.com>
2019-03-19 00:01:37 +0200

    week7-contoso-university

---
+++ week7/ContosoUniversityRazorProject/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUniversityRazorProject.Models;
+
+namespace ContosoUniversityRazorProject.Pages.Courses
+{
+    public class CreateModel : DepartmentNamePageModel
+    {
+        private readonly ContosoUniversityRazorProject.Models.SchoolContext _context;
+
+        public CreateModel(ContosoUniversityRazorProject.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            PopulateDepartmentsDropDownList(_context);
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var emptyCourse = new Course();
+
+            if (await TryUpdateModelAsync<Course>(
+                emptyCourse,
+                "course",   // Prefix for form value.
+                s => s.CourseID, s => s.DepartmentID, s => s.Title, s => s.Credits))
+            {
+                _context.Courses.Add(emptyCourse);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            // Select DepartmentID if TryUpdateModelAsync fails.
+            PopulateDepartmentsDropDownList(_context, emptyCourse.DepartmentID);
+            return Page();
+        }
+    }
+}
---
+++ week7/ContosoUniversityRazorProject/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversityRazorProject.Models;
+
+namespace ContosoUniversityRazorProject.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversityRazorProject.Models.SchoolContext _context;
+
+        public IndexModel(ContosoUniversityRazorProject.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
